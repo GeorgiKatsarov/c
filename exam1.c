29f719b776ebb6a5ff356038204912971fb2ca05
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2025-02-19 15:51:39 +0200

    some more functionality in the practice app exam1, which was my python coding exam

--- exam1.c
+++ exam1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
     //getting the size of the list
@@ -7,16 +9,61 @@ int main(){
     while (flag == 0)
     {
         scanf("%d", &n);
-        if (n > 25 || n < 45)
+        if (n > 25 && n < 45)
         {
             flag = 1;
         }
+        else {
+            printf("\n n must be between 25 and 45!");
+        }
+        
         
     }
+    //declaration of the list
     int list1[45];
-    list1[1] = 1;
-    printf("%d", list1[1]);
-
+    //filling the list with rand integers
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        int num = rand();
+        list1[i] = num;
+        printf("%d  ", num); //peinting the numbers to see them
+    }
+    //finding the count of nums devisable by 3 which are even
+    int countEvenDevByThree = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (list1[i]%2 == 0 && list1[i]%3 == 0)
+        {
+            countEvenDevByThree++;
+        }
+        
+    }
+    //sorting the array
+    int temp;
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 1; i < n; i++)
+        {
+            temp = list1[i];
+            if (list1[i]<list1[i-1])
+            {
+                list1[i] = list1[i-1];
+                list1[i-1] = temp;
+            }
+        
+        }
+    }
+    
+    
+    printf("\n Sorted array");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", list1[i]);
+    }
+    printf("\n Number of even numbers devisable by 3: %d", countEvenDevByThree);
+    
+    
     
     return 0;
 }
