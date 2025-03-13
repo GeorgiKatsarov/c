b0002474d49e79c079d4d04c0a8ab18f38ee697a
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2025-03-13 11:19:14 +0200

    .

---
+++ laboratorno4/1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int getAvg(int arr[], int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return (double)sum/n;
+}
+int main(){
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+    avg = getAvg(balance, 5);
+    printf("Average value is: %.2f", avg);
+    return 0;
+}
