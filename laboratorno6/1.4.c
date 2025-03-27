b58b15851ec0063a9c4948d8a862960d4b703838
GeorgiKatsarov <100018528+GeorgiKatsarov@users.noreply.github.com>
2025-03-27 09:01:03 +0200

    small mistake

--- laboratorno6/1.4.c
+++ laboratorno6/1.4.c
@@ -3,13 +3,13 @@
 #include<time.h>
 int main(){
     int arr[10][10];
-    //pulnim s random chisla
+    //pulnim s random chisla ot 1 do 10
     srand(time(NULL));
     for (int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            arr[i][j] = rand() % 100;
+            arr[i][j] = rand() % 10 + 1;
         }
         
     }
